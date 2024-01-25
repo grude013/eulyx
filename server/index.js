f246b598e8e6c34531de3d4cd8ac2914f95f78e8
grude013 <grude013@umn.edu>
2024-01-25 10:16:58 -0600

    created express middelware to prepend /api

--- server/index.js
+++ server/index.js
@@ -1,18 +1,28 @@
+
 const express = require("express");
-const app = express();
 const cors = require("cors");
+
+// app: main express application wrapper
+// api: express application for /api/**
+const app = express();
+const api = express();
+
 app.use(cors());
+// prepend /api to all routes
+app.use("/api", api);
 
 const port = process.env.PORT || 3500; // Get environment variable PORT from process.yml configuration.
 
-app.get("/", (req, res) => {
+// api route /
+api.get("/", (req, res) => {
     res.json({ msg: "Hello World!" });
 });
-
-app.get("/api", (req, res) => {
+// api route /api
+api.get("/api", (req, res) => {
     res.json({ msg: "Hello API!" });
 })
 
+// listen on port for main express application
 app.listen(port, () => {
     console.log("Server is running on port " + port + "...");
 });
